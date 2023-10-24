52ca14e6d4ba03254c942822b30a38720e329dbc
Nitesh <niteshsomvanshi.it@gmail.com>
2023-10-24 18:23:36 +0200

    Initial DockerFile
    Initial DockerFile

--- src/main/java/com/munichwarriors/manage/ManageMunichWarriorsApp.java
+++ src/main/java/com/munichwarriors/manage/ManageMunichWarriorsApp.java
@@ -6,8 +6,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
